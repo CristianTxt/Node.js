628b5e9eb790ea1aafb896930ccd2e6c9f2b8efc
CristianTxt <79579732+CristianTxt@users.noreply.github.com>
2024-01-15 13:22:29 +0100

    Added new app.mjs

---
+++ app.mjs
@@ -0,0 +1,5 @@
+import chalk from 'chalk';
+
+const log = console.log; 
+
+log(chalk.blue('Why ') + chalk.magenta('did ') + chalk.cyan('The ') + chalk.green('teacher ') + chalk.greenBright(' Go ') + chalk.magentaBright('To ' ) +  chalk.blueBright('Space ') +  chalk.red('? ') + chalk.yellowBright('to ') + chalk.redBright('improve ') + chalk.blueBright('her ') + chalk.black('class ')  + chalk.yellowBright(' Gravity ') + chalk.bgRed('of ') + chalk.whiteBright('understanding '));     
